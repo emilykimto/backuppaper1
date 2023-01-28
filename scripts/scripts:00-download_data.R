a3c5201cb4118416686e9ecd076d2dfd6b3ccbd4
Emily Kim <emilyuna.kim@mail.utoronto.ca>
2023-01-28 18:31:25 -0500

    Add Preamble and comments

--- scripts/scripts:00-download_data.R
+++ scripts/scripts:00-download_data.R
@@ -1,10 +1,30 @@
+#### Preamble ####
+# Purpose: Clean Language Services data downloaded from Toronto Open Data
+# Author: Emily Kim
+# Date: 28 January 2023
+# Contact: emilyuna.kim@mail.utoronto.ca
+# License: MIT
+# Pre-req: None
+
+#### Workspace Set-up ####
+# install.packages("opendatatoronto")
+# install.packages("tidyverse")
+# install.packages("ggplot2")
+# install.packages("knitr")
+# install.packages("janitor")
+# install.packages("lubridate")
+# install.packages("tibble")
+# install.packages("dplyr")
+
 library(knitr)
 library(janitor)
 library(lubridate)
 library(opendatatoronto)
 library(tidyverse)
+library(dplyr)
+library(ggplot2)
 
-#### Acquire ####
+#### Acquire the dataset from Toronto Open Data ####
 language_services <-
   # https://open.toronto.ca/dataset/paramedic-services-911-language-interpretation/
   list_package_resources("42315239-36a8-4b7f-b2ab-6ab60fb0b935") |>
@@ -12,6 +32,7 @@ language_services <-
            "Language Services Data (2014 - 2021)") |>
   get_resource()
 
+#### Saving the dataset ####
 write_csv(
   x = language_services,
   file = "language_services.csv"
@@ -19,12 +40,14 @@ write_csv(
 
 head(language_services)
 
+#### Clean names and reduce data to relevant columns ####
 language_services_clean <-
   clean_names(language_services) |>
   select(time_stamp, language, duration)
 
 head(language_services_clean)
 
+#### Saving cleaned dataset ####
 write_csv(
   x = language_services_clean,
   file = "cleaned_language_services.csv"
