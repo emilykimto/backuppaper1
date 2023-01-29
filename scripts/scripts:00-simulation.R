36464dcbc9868a67644ea7af13b6a2dc794a4ff4
Emily Kim <emilyuna.kim@mail.utoronto.ca>
2023-01-29 16:44:24 -0500

    Edits to data simulation
    Adjusted time frame from 2021 year to 2014-2021

--- scripts/scripts:00-simulation.R
+++ scripts/scripts:00-simulation.R
@@ -8,13 +8,11 @@
 #### Workspace set-up ####
 install.packages("opendatatoronto")
 install.packages("lubridate")
-install.packages("knitr")
 install.packages("tidyverse")
 
-library(knitr)
+library(opendatatoronto)
 library(janitor)
 library(lubridate)
-library(opendatatoronto)
 library(tidyverse)
 
 #### Simulate ####
@@ -22,17 +20,21 @@ set.seed(853)
 
 simulated_languageduration_data <-
   tibble(
-    date = rep(x = as.Date("2021-01-01") + c(0:364), times = 3),
+    date = rep(x = as.Date("2014-01-01") + c(0:364*7), times = 3),
     language = c(
-      rep(x = "Language 1", times = 365),
-      rep(x = "Language 2", times = 365),
-      rep(x = "Language 3", times = 365)
+      rep(x = "Language 1", times = 365*8),
+      rep(x = "Language 2", times = 365*8),
+      rep(x = "Language 3", times = 365*8)
     ),
     call_duration =
       rpois(
-        n = 365 * 3,
+        n = 365*8*3,
         lambda = 15
       )
   )
 
 head(simulated_languageduration_data)
+
+# This will generate a simulated dataset with a constant number of calls and
+# constant language distribution over the 8 years between 2014-2021, which will
+# not be representative of true data.
